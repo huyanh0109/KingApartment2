7b88c6278e2864d361fe325101c93c487d5ae824
huyanh0109 <huyanh0109@gmail.com>
2022-09-21 13:40:47 +0700

    intro can next

---
+++ app/src/main/java/com/example/datn/IntroAdapter.java
@@ -0,0 +1,63 @@
+package com.example.datn;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class IntroAdapter extends RecyclerView.Adapter<IntroAdapter.ViewHolder> {
+    Context context;
+    public IntroAdapter (Context context){
+        this.context=context;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view  =LayoutInflater.from(parent.getContext()).inflate(R.layout.viewpager_intro,parent,false);
+
+        return new IntroAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        switch (position){
+            case 0:
+                holder.tv_title.setText("@string/title_intro_1");
+                holder.tv_content.setText("@string/content_intro");
+                break;
+            case 1:
+                holder.tv_title.setText("@string/title_intro_1");
+                holder.tv_content.setText("@string/content_intro2");
+                break;
+            case 2:
+                holder.tv_title.setText("@string/title_intro_1");
+                holder.tv_content.setText("@string/content_intro");
+                break;
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+    public class ViewHolder  extends RecyclerView.ViewHolder{
+        AppCompatTextView tv_title,tv_content;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tv_title = itemView.findViewById(R.id.tv_title_intro);
+            tv_content = itemView.findViewById(R.id.tv_content_intro);
+        }
+    }
+}
--- app/src/main/java/com/example/datn/MainActivity.java
+++ app/src/main/java/com/example/datn/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.datn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import me.relex.circleindicator.CircleIndicator3;
 
 public class MainActivity extends AppCompatActivity {
-
+    public  static  ViewPager2 vpg_intro;
+    Button btn_next_intro;
+    IntroAdapter introAdapter;
+    CircleIndicator3 circleIndicator3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sheet_intro);
+        getSupportActionBar().hide();
+        btn_next_intro = findViewById(R.id.btn_intro);
+        vpg_intro = findViewById(R.id.vpg_intro);
+        introAdapter = new IntroAdapter(this);
+        vpg_intro.setAdapter(introAdapter);
+        circleIndicator3 = findViewById(R.id.icon_next);
+        circleIndicator3.setViewPager(vpg_intro);
+        btn_next_intro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vpg_intro.setCurrentItem(getItem(+1),true);
+            }
+        });
+    }
+    private int getItem(int i ){
+        return vpg_intro.getCurrentItem()+i;
     }
 }

c8f9edf991824033187057a21c3e1c448c5053b3
QuangHuy <huyanh0109@gmail.com>
2022-12-16 18:30:00 +0700

    update notification

--- app/src/main/java/com/example/datn/fragment/FragmentProfileNotification.java
+++ app/src/main/java/com/example/datn/fragment/FragmentProfileNotification.java
@@ -21,8 +21,10 @@ import com.example.datn.R;
 
 public class FragmentProfileNotification extends Fragment {
     Bundle bundle = new Bundle();
-    SwitchCompat sw_dark;
-    SharedPreferences sharedPreferences = null;
+    SwitchCompat sw_dark,sw_notifi,sw_ring;
+    SharedPreferences sharedPreferencesDark = null;
+    SharedPreferences sharedPreferencesNotifi = null;
+    SharedPreferences sharedPreferencesRing = null;
 
     @Nullable
     @Override
@@ -31,17 +33,21 @@ public class FragmentProfileNotification extends Fragment {
         bundle.putString("Callback", "Profile");
         initView(view);
         dark();
+        notifi();
+        ring();
         return view;
     }
 
     public void initView(View view) {
         sw_dark = view.findViewById(R.id.switch_dark);
+        sw_notifi = view.findViewById(R.id.switch_ads);
+        sw_ring= view.findViewById(R.id.switch_bell);
     }
 
     public void dark() {
-        sharedPreferences = getActivity().getSharedPreferences(AloneMain.NAME_NIGHTLIGHT, Context.MODE_PRIVATE);
-        Boolean bl = sharedPreferences.getBoolean(AloneMain.KEY_BL_NIGHTLIGHT, false);
-        if (bl == true) {
+        sharedPreferencesDark = getActivity().getSharedPreferences(AloneMain.NAME_NIGHTLIGHT, Context.MODE_PRIVATE);
+        Boolean dark = sharedPreferencesDark.getBoolean(AloneMain.KEY_BL_NIGHTLIGHT, false);
+        if (dark == true) {
             sw_dark.setChecked(true);
         }
         sw_dark.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -50,14 +56,60 @@ public class FragmentProfileNotification extends Fragment {
                 if (ba == true) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                     sw_dark.setChecked(true);
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putBoolean("dark_mode", true);
+                    SharedPreferences.Editor editor = sharedPreferencesDark.edit();
+                    editor.putBoolean(AloneMain.KEY_BL_NIGHTLIGHT, true);
                     editor.commit();
                 } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                     sw_dark.setChecked(false);
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putBoolean("dark_mode", false);
+                    SharedPreferences.Editor editor = sharedPreferencesDark.edit();
+                    editor.putBoolean(AloneMain.KEY_BL_NIGHTLIGHT, false);
+                    editor.commit();
+                }
+            }
+        });
+    }
+    public void notifi(){
+        sharedPreferencesNotifi = getActivity().getSharedPreferences(AloneMain.NAME_NOTIFICATION , Context.MODE_PRIVATE);
+        Boolean notifi = sharedPreferencesNotifi.getBoolean(AloneMain.KEY_BL_NOTIFICATION,true);
+        if (notifi == true){
+            sw_notifi.setChecked(true);
+        }
+        sw_notifi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true){
+                    sw_notifi.setChecked(true);
+                    SharedPreferences.Editor editor = sharedPreferencesNotifi.edit();
+                    editor.putBoolean(AloneMain.KEY_BL_NOTIFICATION, true);
+                    editor.commit();
+                }else {
+                    sw_notifi.setChecked(false);
+                    SharedPreferences.Editor editor = sharedPreferencesNotifi.edit();
+                    editor.putBoolean(AloneMain.KEY_BL_NOTIFICATION, false);
+                    editor.commit();
+                }
+            }
+        });
+    }
+    public void ring(){
+        sharedPreferencesRing = getActivity().getSharedPreferences(AloneMain.NAME_RING , Context.MODE_PRIVATE);
+        Boolean ring = sharedPreferencesRing.getBoolean(AloneMain.KEY_BL_RING,true);
+        if (ring == true){
+            sw_ring.setChecked(true);
+        }
+        sw_ring.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true){
+                    sw_ring.setChecked(true);
+                    SharedPreferences.Editor editor = sharedPreferencesRing.edit();
+                    editor.putBoolean(AloneMain.KEY_BL_RING, true);
+                    editor.commit();
+                }else {
+                    sw_ring.setChecked(false);
+                    SharedPreferences.Editor editor = sharedPreferencesRing.edit();
+                    editor.putBoolean(AloneMain.KEY_BL_RING, false);
                     editor.commit();
                 }
             }
